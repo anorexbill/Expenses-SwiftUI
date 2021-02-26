e6dbab58cec9fd08927bc5df7429fd9f4bf128b4
anorexbill <bryteanorchie1@gmail.com>
2021-02-26 01:52:19 -0700

    Initial Commit

---
+++ BuiildThatApp/BuiildThatAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BuiildThatAppApp.swift
+//  BuiildThatApp
+//
+//  Created by Frimpong Anorchie II on 26/02/2021.
+//
+
+import SwiftUI
+
+@main
+struct BuiildThatAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BuiildThatApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BuiildThatApp
+//
+//  Created by Frimpong Anorchie II on 26/02/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
